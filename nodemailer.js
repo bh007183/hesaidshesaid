6d40fcdfe34942af88b7c05d5e2ae4a8217ed1c6
Ben Hopkins <bjhops17@gmail.com>
2025-01-22 10:54:59 -0800

    added nodemailer

---
+++ nodemailer.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+require('dotenv').config()
+const transporter = nodemailer.createTransport({
+  service:'gmail',
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: process.env.EMAIL, // sender address
+    to: process.env.EMAIL, // list of receivers
+    subject: "From Yourself", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
